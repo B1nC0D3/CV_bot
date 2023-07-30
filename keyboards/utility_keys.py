2a66df3847c9d679b1df544e8b1258f3e02cf223
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-07-30 12:51:42 +0300

    typo fix

--- keyboards/utility_keys.py
+++ keyboards/utility_keys.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Та же ситуация что и в keys, но у нас отдельный случай, поэтому решил вынести в отдельный файл
+# Та же ситуация, что и в keys, но у нас отдельный случай, поэтому решил вынести в отдельный файл
 start_buttons = (
     KeyboardButton(text='Обо мне'),
     KeyboardButton(text='Войсы'),

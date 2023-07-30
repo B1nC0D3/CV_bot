720d187e764711412f2bc4d144f93a996e56986c
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-07-30 13:22:03 +0300

    fixed typo in path to files

--- settings.py
+++ settings.py
@@ -8,9 +8,9 @@ TG_TOKEN = os.getenv('TG_TOKEN')
 PROJECT_ROOT = pathlib.Path(os.path.abspath(os.curdir))
 MEDIA_ROOT = PROJECT_ROOT / './media'
 
-LAST_PHOTO = PROJECT_ROOT / './photos/last_photo.jpg'
-SCHOOL_PHOTO = PROJECT_ROOT / './photos/school_photo.jpg'
+LAST_PHOTO = MEDIA_ROOT / './photos/last_photo.jpg'
+SCHOOL_PHOTO = MEDIA_ROOT / './photos/school_photo.jpg'
 
-GPT_VOICE = PROJECT_ROOT / './voices/gpt.ogg'
-SQL_VOICE = PROJECT_ROOT / './voices/slq_vs_nosql.ogg'
-FIRST_LOVE_VOICE = PROJECT_ROOT / './voices/first_love.ogg'
+GPT_VOICE = MEDIA_ROOT / './voices/gpt.ogg'
+SQL_VOICE = MEDIA_ROOT / './voices/slq_vs_nosql.ogg'
+FIRST_LOVE_VOICE = MEDIA_ROOT / './voices/first_love.ogg'
